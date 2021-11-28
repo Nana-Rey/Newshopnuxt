50275c7bbc1cf8bcab453da34737642e5f06c01b
Nana-Rey <68806347+Nana-Rey@users.noreply.github.com>
2021-11-28 23:31:49 +0900

    Vuex

---
+++ newshop/components/vuexcomponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      <v-btn @click="$emit('increase')">
+          増加
+      </v-btn>
+      <v-btn @click="$emit('decrease')">
+          減少
+      </v-btn>
+      {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        counter: Number
+    },
+
+}
+</script>
+
+<style>
+
+</style>
